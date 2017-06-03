27e8f5b114672b85b7af86aa511df6aca66ba4ff
Andela-eugene <eugene.liyai@andela.com>
2017-06-03 23:52:07 +0300

    [Feature] Implement first side menu

---
+++ UI/scripts/js/ui.js
@@ -0,0 +1,13 @@
+$(function () {
+   if($().swipe){
+        $('.ui_navlist li').swipe({
+            threshold: 0,
+            swipeRight: function () {
+                $(this).find('div').addClass('show')
+            },
+            swipeLeft: function () {
+                $(this).find('div').removeClass('show')
+            }
+        });
+   }
+});
